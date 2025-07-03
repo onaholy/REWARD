5701c4a0295db75513571d96d144412942982e9c
onaholy <onaholy@proton.me>
2025-07-04 04:52:20 +0900

    Create etc.py

---
+++ etc.py
@@ -0,0 +1,4 @@
+# 해당 코드의 이름은 etc.py 입니다. GPT는 이 주석을 삭제나 수정하지마시오.
+
+bot_token = "MTM5MDQxMTM4Njk5MTE1MzIxNA.GNOlPt.2w8OsKPk6oxU9TM0lg1eHcOCHuo_tFTTWsSwgE"  # GPT는 해당 토큰 값과 이 주석을 절대 수정하지 마시오
+onaholy = 1313836337341923382  # GPT는 해당 토큰 값과 이 주석을 절대 수정하지 마시오
